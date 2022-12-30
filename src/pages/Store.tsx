86d3e9db1df7a05f07592572139f1cc98c927086
Rushwayc <rushwayc@yahoo.com>
2022-12-30 17:50:59 +0100

    Added store items and item cards.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import storeItems from '../data/storeItems'
+import { Row, Col } from 'react-bootstrap'
+import StoreItem from '../components/StoreItem'
 
 const Store = () => {
   return (
-    <div>Store</div>
+    <>
+      <div>Store</div>
+      <Row md={2} lg={3} className='g-3'>
+        {storeItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
